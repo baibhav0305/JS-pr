194153484a3714d6fb8c5b0f4916cd7d203e7d7a
Baibhav Panda <baibhav030501@gmail.com>
2021-08-16 20:32:13 +0530

    Add files via upload

---
+++ Calculator/js/index.js
@@ -0,0 +1,38 @@
+
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for (item of buttons) {
+
+    item.addEventListener("click", (event) => {
+
+        buttonText = event.target.innerText;
+        // console.log(buttonText);
+        // console.log(buttons);
+
+        if (buttonText == 'x') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == "Back") {
+            // console.log("backspace");
+            screenValue = screenValue.slice(0, screenValue.length - 1);
+            // console.log(screenValue);
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            let answer = eval(screenValue);
+            screen.value = answer;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
