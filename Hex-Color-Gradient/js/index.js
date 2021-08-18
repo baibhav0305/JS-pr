f0e8e7dedeca2d18cd1f4e3911c9685ce2ed05ff
Baibhav <baibhav030501@gmail.com>
2021-08-18 22:43:17 +0530

    Hx-color-gradient files upload

---
+++ Hex-Color-Gradient/js/index.js
@@ -0,0 +1,22 @@
+
+document.getElementById("submit").addEventListener("click", () => {
+
+    var hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+    var hex1 = "";
+    var hex2 = "";
+    var random_index = 0;
+
+    for (let i = 0; i < 6; i++) {
+        random_index = Math.floor(Math.random() * 16);
+        hex1 += hex_numbers[random_index];
+        random_index = Math.floor(Math.random() * 16);
+        hex2 += hex_numbers[random_index];
+    }
+
+    document.body.style.background = `linear-gradient(to right, #${hex1}, #${hex2})`;
+    document.querySelector("button").style.background = `linear-gradient(to right, #${hex2}, #${hex1})`;
+    
+    document.getElementById("hex1").textContent = hex1;
+    document.getElementById("hex2").textContent = hex2;
+
+});
