d4d9475a257775d2e720e8140376612b68ea9f77
Baibhav <baibhav030501@gmail.com>
2021-08-18 23:09:30 +0530

    modified css and js

--- Hex-Color-Gradient/js/index.js
+++ Hex-Color-Gradient/js/index.js
@@ -4,6 +4,8 @@ document.getElementById("submit").addEventListener("click", () => {
     var hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     var hex1 = "";
     var hex2 = "";
+    var hex3 = "";
+    var hex4 = "";
     var random_index = 0;
 
     for (let i = 0; i < 6; i++) {
@@ -13,9 +15,16 @@ document.getElementById("submit").addEventListener("click", () => {
         hex2 += hex_numbers[random_index];
     }
 
+    for (let i = 0; i < 6; i++) {
+        random_index = Math.floor(Math.random() * 16);
+        hex3 += hex_numbers[random_index];
+        random_index = Math.floor(Math.random() * 16);
+        hex4 += hex_numbers[random_index];
+    }
+
     document.body.style.background = `linear-gradient(to right, #${hex1}, #${hex2})`;
-    document.querySelector("button").style.background = `linear-gradient(to right, #${hex2}, #${hex1})`;
-    
+    document.querySelector("button").style.background = `linear-gradient(to right, #${hex3}, #${hex4})`;
+
     document.getElementById("hex1").textContent = hex1;
     document.getElementById("hex2").textContent = hex2;
 
